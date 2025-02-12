001f0bf7fdaa676433bd86dc5aa48bec7acb9f4b
Valentin Serassio <valentinserassio@gmail.com>
2025-02-12 00:55:32 -0300

    creacion nueva hoja

--- 10-curso-js/javascript/clase-31-a-40.js
+++ 10-curso-js/javascript/clase-31-a-40.js
@@ -483,7 +483,6 @@ const arrayAlCuadrado = array => {
     array.forEach(el=>arrayP2.push(el*el))
     return arrayP2
 }
-
 console.log(arrayAlCuadrado([2,4,5]))
 
 
@@ -508,7 +507,6 @@ const minMax = array => {
         numMin
     }
 }
-
 console.log(minMax([1,2,3,4,-2345,5,0,345]))
 //23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
 
@@ -525,13 +523,4 @@ const arrayParImpar = arreglo => {
     arreglo.forEach(el => {if(el%2 == 0){arrayPar.push(el)}else{arrayImpar.push(el)}})
     return {arrayPar,arrayImpar}
 }
-
 console.log(arrayParImpar([1,2,3,4,5,6,7,8,9,0,29]))
-console.log(arrayParImpar())
-
-//Clase 41 - Ejercicios ( 7 / 10 )
-
-//24) Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente, pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }.
-//25) Programa una función que dado un arreglo de elementos, elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devolverá ["x", 10, 2, "10", true].
-//26) Programa una función que dado un arreglo de números obtenga el promedio, pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5.
-
