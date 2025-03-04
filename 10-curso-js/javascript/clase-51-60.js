87f139e0b713d68aa76d01956c6bb0880346e19b
DiolioF <valentinserassiowork@gmail.com>
2025-03-04 00:02:58 -0300

    clase 51 terminada

--- 10-curso-js/javascript/clase-51-60.js
+++ 10-curso-js/javascript/clase-51-60.js
@@ -1,1 +1,49 @@
 //Clase 51 - Maps
+/*
+let mapa = new Map()
+
+mapa.set("nombre","valentin")
+mapa.set("apellido","serassio")
+mapa.set("edad",35)
+
+
+console.log(mapa)
+//saber el tamaño del map
+console.log(mapa.size)
+//saber con true o false si contiene la clave pasada como valor
+console.log(mapa.has("correo"))
+console.log(mapa.has("edad"))
+//devolver el valor de una llave, en caso de no estar devuelve undefined
+console.log(mapa.get("patas"))
+console.log(mapa.get("edad"))
+//para eliminar alguna llave valor usamos .delete() con la llave dentro
+mapa.delete("edad")
+console.log(mapa)
+//Ademas de tener llaves strings, podemos tener llaves con number, null, undefined o cualquier otro tipo de dato
+mapa.set(19,"diecinueve")
+mapa.set({},{})
+console.log(mapa)
+//para recorrerlo debemos usar un for of
+for(let [key,value] of mapa){
+    console.log(`llave ${key}, value ${value}`)
+}
+//para declarar un mapa completo sin usar .set(llave,valor) tenemos que pasar
+//las claves y valores dentro de un array, separando cada llave y valor dentro de [], asi:
+const mapa2 = new Map([
+["nombre","kenai"],
+["edad",7],
+["animal","perro"],
+[null,"nulo"]
+])
+
+console.log(mapa2)
+//para sacar las llaves o valores por separado en un Array usamos el motod .keys() o 
+// .values() y descomponemos lo que nos arroja dentro de un array
+
+const llavesMapa2 = [...mapa2.keys()]
+const valoresMapa2 = [...mapa2.values()]
+
+*/
+
+//clase 52 -
+
